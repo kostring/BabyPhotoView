0760b467e0c6618ec8ed31c6b6103a85037b8ca7
kostring <iamzdzh@126.com>
2018-04-22 22:55:00 +0800

    Add code to dump request body info

--- babyphotoviewer.go
+++ babyphotoviewer.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"net/http"
+	"log"
 )
+
+func photoViewServer(w http.ResponseWriter, req *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	reqBody := buf.String()
+	fmt.Print("Body:\n")
+	fmt.Print(reqBody)
+	fmt.Print("\nBody end\n")
+
+	io.WriteString(w, "Welcome to photo viewer!\n")
+}
 
 func main() {
 	fmt.Printf("Welcome to baby photo view backend!")
+
+	http.HandleFunc("/weichat", photoViewServer)
+	err := http.ListenAndServe(":80", nil)
+	if err !=  nil {
+		log.Fatal(err)
+	}
 }
