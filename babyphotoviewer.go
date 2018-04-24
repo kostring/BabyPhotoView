291e0fd65b7bfbc6d3c4264c39ea0581f34083dc
kostring <iamzdzh@126.com>
2018-04-24 22:42:32 +0800

    Add code to support weichat verification

--- babyphotoviewer.go
+++ babyphotoviewer.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"errors"
 	"bytes"
+	"sort"
+	"net/url"
 	"fmt"
-	"io"
 	"net/http"
 	"log"
+	"crypto/sha1"
 )
 
-func photoViewServer(w http.ResponseWriter, req *http.Request) {
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(req.Body)
-	reqBody := buf.String()
-	fmt.Print("Body:\n")
-	fmt.Print(reqBody)
-	fmt.Print("\nBody end\n")
+const (
+	token = "kostring112233"
+)
 
-	io.WriteString(w, "Welcome to photo viewer!\n")
+func processVerification(parameters url.Values) ([]byte, error) {
+	inputArgs := [3]string{token, parameters["timestamp"][0], parameters["nonce"][0]}
+	argsSlice := inputArgs[:]
+
+	sort.Strings(argsSlice)
+	var buf bytes.Buffer
+	for _, s := range argsSlice {
+		buf.WriteString(s)
+	}
+
+	sha1Result := sha1.Sum(buf.Bytes())
+
+	//Reuse buf to get the correct sha1 result string
+	buf.Reset()
+
+	for _, b := range sha1Result {
+		buf.WriteString(fmt.Sprintf("%.2x", b))
+	}
+
+	sha1String := buf.String()
+
+	if sha1String != parameters["signature"][0] {
+		log.Printf("Invalid verification request! Signature %s, expect: %s", parameters["signature"][0], sha1String)
+		return nil, errors.New("Invalid verification request!")
+	}
+
+	return []byte(parameters["echostr"][0]), nil
+}
+
+func photoViewServer(w http.ResponseWriter, req *http.Request) {
+	if req.ParseForm() != nil {
+		log.Fatal("Failed to parse req!")
+	}
+
+	parameters := req.Form
+	returnInfo, err := processVerification(parameters)
+	if err != nil {
+		return
+	}
+	w.Write(returnInfo)
 }
 
 func main() {
